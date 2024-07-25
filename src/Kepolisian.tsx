74cdeadc8469f5256d820159b85dfa9efa201eaf
irfansatya <irfansatya75@gmail.com>
2024-07-25 15:23:44 +0700

    update8

---
+++ src/Kepolisian.tsx
@@ -0,0 +1,12 @@
+import { Component, createSignal, createEffect } from 'solid-js';
+import styles from './Kepolisian.module.css';
+import logo from './image/logo.svg';
+import notifikasi from './image/notifikasi.svg';
+import riwayat from './image/riwayat.svg';
+import search from './image/search.svg';
+import iconBeranda from './image/iconBeranda.svg';
+import logoKepolisian from './image/logoKepolisian.svg';
+import iconTelepon from './image/iconTelepon.svg';
+import tambahLokasi from './image/tambahLokasi.svg';
+import iconTeleponHover from './image/iconTeleponHover.svg';
+import tambahLokasiHover from './image/tambahLokasiHover.svg';
